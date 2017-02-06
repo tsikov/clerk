38785dba61d74e767ad0fedbd0e1b78bb066a163
Petko Tsikov <tsikov@gmail.com>
2017-02-06 11:21:11 +0200

    Initial implementation of execution of events.

--- src/clerk.lisp
+++ src/clerk.lisp
@@ -3,19 +3,24 @@
   (:use #:cl)
   (:export #:*events*
            #:empty-events-queue
-           #:event))
+           #:event
+           #:start
+           #:stop))
 (in-package #:clerk)
 
 ;; TODO:
 ;; display calendar
+;; rename event -> job
 
 (defparameter *events* nil)
+(defparameter *fired-events* nil)
+(defparameter *main-thread* nil)
 
 (defclass event ()
-  ((name :initarg :name)
+  ((name :initarg :name :reader name)
    (interval :initarg :interval)
    (fire-time :initarg :fire-time :reader fire-time)
-   (body :initarg :body)))
+   (body :initarg :body :reader body)))
 
 (defclass continuous-event (event) ())
 (defclass one-time-event (event) ())
@@ -47,7 +52,55 @@
 (defun add-to-event-queue (name type interval body)
   (let ((event (make-event name type interval body)))
     (push event *events*)
-    (sort *events* #'< :key #'fire-time)))
+    (sort *events* #'< :key #'fire-time)
+    event))
      
 (defun empty-events-queue ()
   (setf *events* nil))
+
+(defun empty-fired-events-list ()
+  (setf *fired-events* nil))
+
+(defun fire-event-p (event)
+  "Check if it is time to fire an event"
+  (<= (fire-time event) (get-universal-time)))
+
+(defmacro add-to-fired-events (name body)
+  ;; TODO: EVAL IS ONLY TEMPORARY!!!
+  `(push (bt:make-thread #'(lambda () (eval ,body))
+                         :name ,name)
+         *fired-events*))
+
+(defmethod fire-event ((event event))
+  (with-slots (name body) event
+    (add-to-fired-events name body)))
+
+(defmethod fire-event :before ((event continuous-event))
+  "Create the next event in the event queue when firing continuous
+events."
+  (with-slots (name interval body) event
+    (add-to-event-queue name 'every interval body)))
+
+(defun fire-event-if-needed ()
+  (if (fire-event-p (car *events*))
+      (progn
+        (fire-event (pop *events*))
+        ;; just in case the second event in queue is the same
+        ;; second as the first one. Or there might be a lot of
+        ;; events in the queue.
+        (fire-event-if-needed))))
+
+(defun start ()
+  "Start the thread that waits for an event to fire."
+  (setf *main-thread*
+        (bt:make-thread
+         #'(lambda ()
+             (loop
+                (fire-event-if-needed)
+                (sleep 1)))
+         :name "Main scheduler thread.")))
+
+(defun stop ()
+  "Stop scheduler."
+  (bt:destroy-thread *main-thread*)
+  (setf *main-thread* nil))
--- t/clerk.lisp
+++ t/clerk.lisp
@@ -22,22 +22,54 @@
         "Can make one-time event"
         :test #'string=))
 
-    (subtest "macro (event ..."
-      (clerk:event "Cool event" every 5.days (print "Party!"))
-      (is (length clerk:*events*)
-          1
-          "Adds an event to the events queue.")
+  (clerk:empty-events-queue)
+  (subtest "macro (event ..."
+    (clerk:event "Cool event" every 5.days (print "Party!"))
+    (is (length clerk:*events*)
+        1
+        "Adds an event to the events queue.")
+    
+    (clerk:empty-events-queue)
+    (clerk:event "First event to fire"
+                 in 1.minute (print "Fire!"))
+    (clerk:event "Second event to fire"
+                 in 2.minutes (print "Fire!"))
+    (with-slots (clerk::name) (first clerk:*events*)
+      (is clerk::name "First event to fire"
+          "Orders events by time of firing."
+          :test #'string=))
+    ;; clear the event queue again for future tests
+    (clerk:empty-events-queue))
+  
+  (subtest "function (fire-event-p"
+    (ok (not (clerk::fire-event-p
+              (make-instance 'clerk:event
+                             :interval '1.minute)))
+        "Event is not fired before it's time")
+    (ok (clerk::fire-event-p
+         (make-instance 'clerk:event
+                        :interval '-1.second))
+        "Event is fired when the time comes"))
 
+  (clerk:empty-events-queue)
+    (clerk::empty-fired-events-list)
+    (subtest "defmethod (fire-event"
+      (clerk::fire-event
+       (clerk:event "One-time event" in 1.second (+ 1 2)))
+      (is (bt:join-thread (car clerk::*fired-events*))
+          3
+          "The event's calculation is performed successfully")
+      (is (length clerk::*fired-events*) 1
+          "The fired events queue has the fired event in it")
+      (is (length clerk:*events*) 1
+          "One-time events don't create a new event in the event queue
+when they are fired.")
       (clerk:empty-events-queue)
-      (clerk:event "First event to fire"
-                   in 1.minute (print "Fire!"))
-      (clerk:event "Second event to fire"
-                   in 2.minutes (print "Fire!"))
-      (with-slots (clerk::name) (first clerk:*events*)
-        (is clerk::name "First event to fire"
-            "Orders events by time of firing."
-            :test #'string=))
-      ;; clear the event queue again for future tests
-      (clerk:empty-events-queue)))
+      (clerk::empty-fired-events-list)
+      (clerk::fire-event
+       (clerk:event "Continuous event" every 1.second (+ 1 2)))
+      (is (length clerk:*events*) 2
+          "Continuous events create a new event in the event queue when
+when they are fired")))
 
 (finalize)

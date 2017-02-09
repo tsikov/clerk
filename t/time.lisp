82aa8c875c298c4d8669b30d177c6f8908400fd6
Petko Tsikov <tsikov@gmail.com>
2017-02-09 10:02:56 +0200

    Add descriptions for some time-related tests.

--- t/time.lisp
+++ t/time.lisp
@@ -6,24 +6,28 @@
 (plan 1)
 
 (subtest "package clerk.time.test"
-
   (is (multiple-value-list
        (clerk.time::split-interval '1.minute))
       '(1 minute)
+      "Can split numbered interval"
       :test #'equalp)
 
   (is (clerk.time::interval-type->seconds 'minute)
-      60)
+      60
+      "Can convert interval-type to seconds")
 
   (is (clerk.time::interval->seconds '2.minutes)
-      120)
+      120
+      "Can convert numbered interval to seconds")
   (is (clerk.time::interval->seconds '1.hour)
-      360)
+      360
+      "Can convert numbered interval to seconds")
 
   (ok (typep (clerk.time::interval->seconds 'friday) 'number)
       "Days of the week return the remaining seconds until this time")
 
   (is (clerk.time:timejump 0 '1.minute)
-      60))
+      60
+      "Can move the time with a numbered interval"))
 
 (finalize)
